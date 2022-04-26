3742ad72f2bf1668a8c6f6fcaec3f1817f196c1c
Jagroop Singh <jsingh8448@gmail.com>
2022-04-25 19:09:54 -0700

    Post-Merge file conflict fix

--- dashboard-website/mortgage_dashboard/frontend/src/index.js
+++ dashboard-website/mortgage_dashboard/frontend/src/index.js
@@ -1,2 +1,7 @@
-import '../static/css/index.css';
-import App from "./components/App";
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+
+const container = document.getElementById('app')
+const root = createRoot(container)
+root.render(<App />);
