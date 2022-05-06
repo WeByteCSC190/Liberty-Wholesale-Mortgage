5ffe964b72dc9f1d243be2c4fc19d07743466b98
Benson Chu <bensonchu@Bensons-MacBook-Pro.local>
2022-05-06 15:10:15 -0700

    Fixed URL pathings and settings

--- dashboard-website/mortgage_dashboard/mortgage_dashboard/urls.py
+++ dashboard-website/mortgage_dashboard/mortgage_dashboard/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard', include('dashboard.urls')),
-    path('', include('frontend.urls')),
+    # path('', include('frontend.urls')),
 
     path('api/', include('api.urls'))
 
