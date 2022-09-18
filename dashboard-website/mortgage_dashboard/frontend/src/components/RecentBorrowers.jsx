633dab7c104b80af0df1bd3346657caceb3f5588
Jagroop Singh <jsingh8448@gmail.com>
2022-09-18 12:02:57 -0700

    Add restyled Dashboard Frontend

--- dashboard-website/mortgage_dashboard/frontend/src/components/RecentBorrowers.jsx
+++ dashboard-website/mortgage_dashboard/frontend/src/components/RecentBorrowers.jsx
@@ -1,48 +1,38 @@
 import * as React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import './RecentBorrowers.css'
 
-const createData = (date, fname, lname) => {
-  return { date, fname, lname };
+const createData = (case_id, date, fname, lname, phone) =>{
+    return { case_id, date, fname, lname, phone };
 }
 
 const rows = [
-  createData('03/01/21', 'Claire', 'Winchester'),
-  createData('06/31/20', 'Dave', 'Crocker'),
-  createData('07/11/19', 'Batman', 'NotWayne'),
+  createData('1000232', '03/01/21', 'Claire', 'Winchester', '9167982333'),
+  createData('1000323', '06/31/20', 'Dave', 'Crocker', '9163839848'),
+  createData('1000324', '07/11/19', 'Batman', 'NotWayne', '9163938484'),
 ];
 
 const RecentBorrowers = () => {
   return (
     <div className="RecentBorrowers">
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="right">Date</TableCell>
-              <TableCell align="right">First Name</TableCell>
-              <TableCell align="right">Last Name</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.date}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                {/* <TableCell component="th" scope="row"> */}
-                {/*   {row.name} */}
-                {/* </TableCell> */}
-                <TableCell align="right">{row.date}</TableCell>
-                <TableCell align="right">{row.fname}</TableCell>
-                <TableCell align="right">{row.lname}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <div className='RecentBorrowersTitle'>Recently Added Borrowers</div>
+      <ol className='RecentBorrowersColumnsNames RecentBorrowersData'>
+        <li>Case ID</li>
+        <li>Date</li>
+        <li>First</li>
+        <li>Last</li>
+        <li>Phone #</li>
+      </ol>
+      {rows.map((row) => (
+        <ol className='RecentBorrowersData'>
+          <li>{row.case_id}</li>
+          <li>{row.date}</li>
+          <li>{row.fname}</li>
+          <li>{row.lname}</li>
+          <li>{row.phone}</li>
+        </ol>
+      ))}
     </div>
   );
-
 }
 
 export default RecentBorrowers;
--- dashboard-website/mortgage_dashboard/frontend/src/pages/Dashboard.jsx
+++ dashboard-website/mortgage_dashboard/frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
+import '../components/Dashboard.css';
 import Navbar from "../components/Navbar"; 
 import Milestone from "../components/Milestone";
 import RecentLeads from "../components/RecentLeads";
 import RecentBorrowers from "../components/RecentBorrowers";
 import AnnouncementsWidget from "../components/AnnouncementsWidget";
+import DashboardCards from "../components/DashboardCards"
 
 const Dashboard = () => {
   return (
     <div className="Dashboard">
       <Navbar />
       <Milestone />
+      <DashboardCards />
       <RecentLeads />
       <RecentBorrowers />
       <AnnouncementsWidget />
