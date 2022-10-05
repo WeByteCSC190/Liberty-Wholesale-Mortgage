ac9821b10bc39b2bb1fce1310119eb592ef4b5d1
Negin Mahrang <neginmahrang@Negins-MacBook-Air.local>
2022-10-04 17:53:27 -0700

    Connected Search box and table data to BackEnd to retrieve data dynamically from the DB. Search functionality is working as the user types.

--- dashboard-website/mortgage_dashboard/frontend/src/pages/Borrowers.jsx
+++ dashboard-website/mortgage_dashboard/frontend/src/pages/Borrowers.jsx
@@ -1,73 +1,46 @@
+import axios from 'axios';
 import * as React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { useState, useEffect } from 'react';
+// import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
-
-
+import Table from "../components/Table";
 const Borrowers = () => {
-  return (
-    <>
-    <div className="Header">
-      <Navbar />
-      <p className="Page-Title">Borrowers</p>
-    </div>
-    <div className="Content">
-      <BorrowersTable />
-    </div>
-    </>
-  )
-}
-export default Borrowers; 
+  const [dataTable, setDataTable] = useState([]);
+  const borrowersUrl = "http://localhost:8000/api/borrowers";
+  function getBorrowers() {
+  axios({
+      method: "GET",
+      url:borrowersUrl,
+    }).then((response)=>{
+      const data = response.data;
+      setDataTable(response.data)
+     console.log(response)
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        }
+    })}
+  useEffect(() => {
+    getBorrowers();
+  }, []);
+  const column = [
+    { heading: 'First Name', value: 'fname' },
+    { heading: 'Last Name', value: 'lname' },
+    { heading: 'Email', value: 'email' },
+    { heading: 'Phone', value: 'phone_num' },
+    { heading: 'Date', value: 'date' },
+  ]
 
-const BorrowersTable = () => {
-  function createData(fname, lname, email, phone_num, date) {
-    return { fname, lname, email, phone_num, date };
-  }
-
-  const rows = [
-    createData('Claire', 'Winchester', 'claire@hotmail.uk', '916-799-0111', '03/01/21'),
-    createData('Dave', 'Crocker', 'odd@hotmail.uk', '916-799-0111', '06/31/20'),
-    createData('Batman', 'NotWayne', 'notwayne@hotmail.uk', '916-799-0111', '07/11/19'),
-    createData('Monsour', 'Friedman', 'mfriedman@hotmail.uk', '916-799-0111', '01/01/18'),
-    createData('Jessica', 'Casa', 'casa@hotmail.uk', '916-799-0111', '04/18/17'),
-    createData('Prenoit', 'Frenchman', 'prenoitf@hotmail.uk', '916-799-0111', '09/21/16'),
-  ];
   return (
     <div className="Borrowers">
-      
-      <Search />
-      <br/>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="right">First Name</TableCell>
-              <TableCell align="right">Last Name</TableCell>
-              <TableCell align="right">Email</TableCell>
-              <TableCell align="right">Phone Number</TableCell>
-              <TableCell align="right">Date</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.date}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                {/* <TableCell component="th" scope="row"> */}
-                {/*   {row.name} */}
-                {/* </TableCell> */}
-                <TableCell align="right">{row.fname}</TableCell>
-                <TableCell align="right">{row.lname}</TableCell>
-                <TableCell align="right">{row.email}</TableCell>
-                <TableCell align="right">{row.phone_num}</TableCell>
-                <TableCell align="right">{row.date}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>   
-       </div>
+      <Navbar />
+      <Search url={borrowersUrl} />
+      <Table data={dataTable} column={column}/>
+      </div>
   );
 }
 
+export default Borrowers;
