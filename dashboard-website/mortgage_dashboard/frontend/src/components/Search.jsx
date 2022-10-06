3c99939e983508aa9c324cf052cce43b1bd28654
Negin Mahrang <neginmahrang@Negins-MacBook-Air.local>
2022-10-05 23:25:49 -0700

    Search box filters table. Previously, the table was not being filtered rather a dropdown was shown for filter data.

--- dashboard-website/mortgage_dashboard/frontend/src/components/Search.jsx
+++ dashboard-website/mortgage_dashboard/frontend/src/components/Search.jsx
@@ -1,88 +1,38 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as Icons from "@fortawesome/free-solid-svg-icons";
-class Search extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      Items: [""],
-      url: props.url
-    }
-    this.node = React.createRef()
+const Search=({callback})=> {  
+  const [innerValue, setInnerValue]=useState("");
+  const handleSubmit = e => {
+    e.preventDefault()
+    callback(innerValue)
   }
-  componentDidMount() {
-    document.addEventListener('mousedown', this.onSearchClick)
-  }
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.onSearchClick)
-  }
-  onSearchClick = (e) => {
-    if (this.node.current.contains(e.target)) {
-      return
-    }
-    this.setState({
-      Items: [],
-    })
-  }
-  handleAPI = async (e) => {
-    await axios
-      .get(this.state.url)      //insert API call here later
-      .then((res) => {
-        this.setState({
-          Items: res.data
-        })
-      })
-      .catch((err) => {
-        alert(err)
-      })
-    let convertToLc = e.target.value.toLowerCase()
-   
-    let filterData = this.state.Items.filter((e) => {
-      let nameToLc = e.fname.toLowerCase()
-       console.log(nameToLc.indexOf(convertToLc))
-      return nameToLc.indexOf(convertToLc) !== -1
-    })
-    console.log(this.state.Items)
-    this.setState({
-      Items: filterData,
-    })
-  }
-  render() {
     return (
       <div className="container mt-5 search-container">
         <Container className="mt-4">
+          <form onSubmit={handleSubmit}>
         <Row>
             <Col sm={10}>
             <div>
             <input
             type="text"
-            onClick={this.onSearchClick}
             className="form-control"
-            onChange={this.handleAPI}
-            placeholder="Search ..."
-            ref={this.node} />
+            value={innerValue}
+            onChange={(e)=> setInnerValue(e.target.value)}
+            />
             </div>
-            <ul className="list-group">
-                {this.state.Items.map((res) => {
-                  console.log(res)
-                return (
-                <a href="/"
-                    className="list-group-item list-group-item-action"
-                    >{res.fname} {res.lname}
-                </a>
-                )
-            })}
-            </ul>
+        
         </Col>
         
         <Col sm={2}>
-            <button className="btn btn-primary"><FontAwesomeIcon icon={Icons.faMagnifyingGlass}  /></button>
+            <button  className="btn btn-primary"><FontAwesomeIcon icon={Icons.faMagnifyingGlass}  /></button>
         </Col>
-        </Row>
+            </Row>
+            </form>
     </Container>
     <Container className="mt-4 mb-4">
     <Row  xs="auto" >
@@ -108,6 +58,5 @@ class Search extends Component {
     </Container>
       </div>
     )
-  }
 }
 export default Search;
