2cfd7db0985fc2150493b1966baf54cb5613f695
Negin Mahrang <neginmahrang@Negins-MacBook-Air.local>
2022-10-13 17:24:25 -0700

    add row and table componenets resolved.

---
+++ dashboard-website/mortgage_dashboard/frontend/src/components/Table.jsx
@@ -0,0 +1,55 @@
+import Table from 'react-bootstrap/Table';
+import AddRow from "../components/modals/AddRow";
+import ActionBtn from "../components/buttons/Action";
+const TableComponent = ({ page, data, column }) => {
+ let pageName = page
+  return (
+    <div style={{ paddingBottom: 300,paddingLeft: 90, paddingRight: 90 }}>
+
+      <Table className="Table" responsive hover >
+      <thead>
+      <tr className="table-title">List of {pageName}</tr>
+      <tr className="table-heading">
+            {column.map((item, index) =>
+              <TableHeadItem item={item} />
+            )}
+        </tr>
+      </thead>
+      <tbody>
+         {data.map((item, index) => <TableRow item={item} column={column} />)}
+      </tbody>
+      <tr className="last-table-row">
+        Showing {data.length} out of {data.length} results 
+        </tr>
+      </Table>
+      </div>
+  );
+}
+const TableHeadItem = ({ item }) =>
+  {
+  if (item.heading === 'AddRow') {
+    return (<th><AddRow /></th>);
+  } else {
+    return (<th>{`${item.heading}`}</th>);
+  }
+};
+
+const TableRow = ({ item, column }) => (
+  <tr>
+    {column.map((columnItem, index) => {
+
+      if(columnItem.value.includes('.')) {
+        const itemSplit = columnItem.value.split('.') //['address', 'city']
+        return <td>{item[itemSplit[0]][itemSplit[1]]}</td>
+      }
+      if (columnItem.heading === 'AddRow') {
+        return (<th><ActionBtn /></th>);
+      } else {
+        return <td>{item[`${columnItem.value}`]}</td>
+      }
+      
+    })}
+  </tr>
+)
+
+export default TableComponent;
