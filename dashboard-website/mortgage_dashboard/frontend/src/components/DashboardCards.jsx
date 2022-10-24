f0d1291d6351b46a6780a836bdb0c5b1e6f375c7
Jagroop Singh <jsingh8448@gmail.com>
2022-10-23 22:19:13 -0700

    CSC190E-197

--- dashboard-website/mortgage_dashboard/frontend/src/components/DashboardCards.jsx
+++ dashboard-website/mortgage_dashboard/frontend/src/components/DashboardCards.jsx
@@ -1,28 +1,75 @@
 import * as React from 'react';
 import './DashboardCards.css'
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const DashboardCards = () => {
   return (
     <div className="DashboardCards">
-      <div className="card">
-        <p className="CardTitle">New Leads</p>
-        <p className="CardFilter">last 7 days^</p>
-        <p className="CardValue">10</p>
+      <div className='dashboard-card'>
+        <div className='card-title'>New Leads</div>
+        <div className='card-filter'>
+
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Filter
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Last 7 Days</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Last 14 Days</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Last 30 Days</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className='card-value'><p>10</p></div>
       </div>
-      <div className="card">
-        <p className="CardTitle">Application Submitted</p>
-        <p className="CardFilter">last 7 days^</p>
-        <p className="CardValue">3</p>
+      <div className='dashboard-card'>
+        <div className='card-title'>Application<br />Submitted</div>
+        <div className='card-filter'>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Filter
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Last 7 Days</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Last 14 Days</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Last 30 Days</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className='card-value'><p>3</p></div>
       </div>
-      <div className="card">
-        <p className="CardTitle">New Borrowers</p>
-        <p className="CardFilter">last 7 days^</p>
-        <p className="CardValue">6</p>
+      <div className='dashboard-card'>
+        <div className='card-title'>New Borrowers</div>
+        <div className='card-filter'>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Filter
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Last 7 Days</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Last 14 Days</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Last 30 Days</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className='card-value'><p>6</p></div>
       </div>
-      <div className="card">
-        <p className="CardTitle">Needs Attention</p>
-        <p className="CardFilter">last 7 days^</p>
-        <p className="CardValue">5</p>
+      <div className='dashboard-card'>
+        <div className='card-title'>Officers Online</div>
+        <div className='card-filter'>
+
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Filter
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Last 7 Days</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Last 14 Days</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Last 30 Days</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className='card-value'><p>5</p></div>
       </div>
     </div>
   );
