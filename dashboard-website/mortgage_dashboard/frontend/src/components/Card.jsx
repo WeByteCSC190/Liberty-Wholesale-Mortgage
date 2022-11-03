4a556ab304b54d6e7a552af68b7677bbf40fd82f
Negin Mahrang <neginmahrang@Negins-MacBook-Air.local>
2022-11-02 17:06:02 -0700

    Created carousel and cards components on resrouces page.

---
+++ dashboard-website/mortgage_dashboard/frontend/src/components/Card.jsx
@@ -0,0 +1,20 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function CardVideo() {
+  return (
+    <Card style={{ }}>
+      <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg" />
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default CardVideo;
