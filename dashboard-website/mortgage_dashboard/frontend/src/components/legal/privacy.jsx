337623d4ba580b322ad9e78006b6c7fbdb00cbd7
Jose Ordaz <73565950+onlineoj@users.noreply.github.com>
2022-12-01 22:34:44 -0800

    CSC190E-382, 383, 385-387: Polishing Legal Links
    Overhauls the legal pages.
    Replaces the HTML version with react-bootstrap cards.

--- dashboard-website/mortgage_dashboard/frontend/src/components/legal/privacy.jsx
+++ dashboard-website/mortgage_dashboard/frontend/src/components/legal/privacy.jsx
@@ -1,53 +1,149 @@
-import React, { Component } from "react";
-import WhiteLogo from "../images/logo.jpg";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import WhiteLogo from '../images/logo.jpg'
 
-export default class Privacy extends Component {
-  render() {
+
+export default function PrivacyPolicy() {
     return (
-      <body className="legal-body">
-        <section className="legal-section">
-          <div className="legal-container">
-            <div className="legal-intro">
-              <img
-                src={WhiteLogo} //MLO Support Logo
-                width="200"
-                height="100"
-                className="company-logo"
-                alt="Logo"
-              />
-              <h1 className="legal-title">MLO SUPPORT PRIVACY POLICY</h1>
-              <p>Last Updated:</p>
-            </div>
-          </div>
-        </section>
+    <Container style={{maxWidth: '800px'}}>
+      <Card border="light">
+        <Card.Body className="company-logo">
+            <img
+            src={WhiteLogo} //MLO Support Logo
+            width="150"
+            height="70"
+            className="Nav-Logo"
+            alt="MLO Support">
+           </img>
+        </Card.Body>
+        <Card.Body>
+          <Card.Title className="mb-4 text-center">MLO Support Privacy Policy</Card.Title>
+          
+          <Card.Subtitle className="mb-2 text-muted">1. General Information</Card.Subtitle>
+          <Card.Text classname="mb-4">
+                This Privacy Policy governs the manner in which loanofficersupport.com 
+                (the “Company”) collects, uses, maintains and discloses information 
+                 collected from users of this Web site (the “Users”). 
+                 Privacy Users privacy is very important to smallbusinesslendingsource.com. 
+                 smallbusinesslendingsource.com is committed to safeguarding the information 
+                 Users entrust to loanofficersupport.com. 
+                 This Web site is not directed at individuals under 18 years of age. 
+          </Card.Text>
 
-        <section className="legal-section">
-          <div className="legal-container">
-            <div className="legal-content">
-              <h3>Heading 1</h3>
-              <p>Content of Heading 1</p>
-            </div>
-          </div>
-        </section>
+          <Card.Subtitle className="mb-2 text-muted">2. Information We Collect</Card.Subtitle>
+          <Card.Text classname="mb-4">
+                 The Information We Collect may collect nonpublic personal information 
+                 from User in a variety of ways, including online forms for ordering products 
+                 and services, and other instances where Users are invited to volunteer such information. 
+                 The type of information we collect from consumers includes, 
+                 but is not limited to: First Name, Last Name, Street Address, City, State, 
+                 Zip Code, Best Time To Call, Daytime Phone, Evening Phone, and Email Address.
+          </Card.Text>
 
-        <section className="legal-section">
-          <div className="legal-container">
-            <div className="legal-content">
-              <h3>Heading 2</h3>
-              <p>Content of heading 2</p>
-            </div>
-          </div>
-        </section>
+          <Card.Subtitle className="mb-2 text-muted">3. Infomation Collected by Third Parties</Card.Subtitle>
+          <Card.Text classname="mb-4">
+                smallbusinesslendingsource.com may also collect information 
+                about how Users use our Web site, 
+                for example, by tracking the number of unique views received by the pages of the Web site 
+                or the domains from which Users originate.
+          </Card.Text>
 
-        <section className="legal-section">
-          <div className="legal-container">
-            <div className="legal-ending">
-              <h3></h3>
-              <p></p>
-            </div>
-          </div>
-        </section>
-      </body>
+          <Card.Subtitle className="mb-2 text-muted">4. Cookie Policy</Card.Subtitle>
+          <Card.Text classname="mb-4">
+                We may use “cookies” to track how Users use our Web site. 
+                A cookie is a piece of software that a Web server can store on the Users’ PC 
+                and use to identify the Users should they visit the Web site again. 
+                While not all of the information that we collect from 
+                Users is nonpublic personal information, 
+                it may be associated with nonpublic personal information 
+                that Users provide us through our Web site.
+          </Card.Text>
+
+          <Card.Subtitle className="mb-2 text-muted">5. How We Use This Information</Card.Subtitle>
+          <Card.Text classname="mb-4">
+                loanofficersupport.com collects nonpublic personal information from its Users 
+                in order to match Users with lenders and brokers.
+                 By clicking the “Submit Form” button on the application, 
+                 Users consent to the disclosure of their nonpublic personal information to these lenders and brokers.
+                 The lenders and brokers may contact you directly 
+                 via phone, email or direct mail after they receive your nonpublic personal information.
+          </Card.Text>
+
+          <Card.Subtitle className="mb-2 text-muted">6. How Third-Parties Use This Information</Card.Subtitle>
+          <Card.Text classname="mb-4">
+                smallbusinesslendingsour.com may also use information collected 
+                through our web site for research regarding the effectiveness 
+                of the web site and Company’s marketing and advertising efforts
+          </Card.Text>
+
+          <Card.Subtitle className="mb-2 text-muted">7. Disclosure of Information to Nonaffiliated Third Parties</Card.Subtitle>
+          <Card.Text classname="mb-4">
+                smallbusinesslendingsource.com may disclose nonpublic personal information about you 
+                to other market participants that provide goods and services. 
+                These nonaffiliated third parties may include, 
+                but are not limited to 
+                (i) financial service providers such as lenders or brokers outside of our network 
+                of lenders and brokers and insurance companies and 
+                (ii) non-financial companies such as retailers and direct marketers. 
+                Smallbusinesslendingsource.com may also disclose aggregate, anonymous data to investors 
+                and potential partners based on information collected from Users.
+          </Card.Text>
+
+          <Card.Subtitle className="mb-2 text-muted">8. Opt-Out of Disclosing to Third Parties</Card.Subtitle>
+          <Card.Text classname="mb-4">
+                Users may opt out of the Company’s disclosure to nonaffiliated third parties 
+                by following the instructions in the opt out section of this privacy policy. 
+                We may also disclose nonpublic personal information about you as permitted by law. 
+                Consent to Receive Privacy Notice and Opt Out Notice Via this Web Site 
+                By acknowledging receipt of this privacy notice, 
+                you the customer also consent to receive your annual notice of the privacy policy, 
+                as required by FTC regulation, via this web site. 
+                By acknowledging receipt of this privacy notice, 
+                you the customer also consent to receive your opt out notice and opportunity to opt out, 
+                as required by FTC regulation, via this web site. 
+          </Card.Text>
+
+          <Card.Subtitle className="mb-2 text-muted">9. Maintenance of Information & Security Information about Users</Card.Subtitle>
+          <Card.Text classname="mb-4">
+                Maintenance of Information & Security Information about Users 
+                that is maintained on our systems is protected using industry standard security measures 
+                that includes but is not limited to security measures in place 
+                to protect the loss, misuse and alteration of the information under our control. 
+                All data is protected using the most advanced methods available. 
+                We do not store financial information, credit card numbers or personal information 
+                like social security numbers on this Web site. 
+                However, due to the nature of the Internet, computer networks, systems and other factors, 
+                we cannot guarantee that the information submitted to, maintained on, 
+                or transmitted from our systems will be completely secure.
+          </Card.Text>
+
+          <Card.Subtitle className="mb-2 text-muted">10. Use of An IP Address</Card.Subtitle>
+          <Card.Text classname="mb-4">
+                An IP (Internet Protocol) address is a number that is assigned to your computer when you use the Internet. 
+                Your IP address does not contain any nonpublic personal information about you. 
+                smallbusinesslendingsource.com collects IP addresses for the purposes of system administration, 
+                to report aggregate information to our advertisers, and to ensure the validity of customer requests. 
+                Unsubscribe If you would like to unsubscribe and opt out, please (send email to
+          </Card.Text>
+         
+
+         <Card.Subtitle className="mb-2 text-muted">11. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+        
+
+         <Card.Subtitle className="mb-2 text-muted">12. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+
+         </Card.Body> 
+         <Card.Text className="mb-2 text-center">MLO Support | A Liberty Wholesale Company</Card.Text>
+      </Card>
+            
+      </Container>
     );
   }
-}
+
--- dashboard-website/mortgage_dashboard/frontend/src/components/legal/terms.jsx
+++ dashboard-website/mortgage_dashboard/frontend/src/components/legal/terms.jsx
@@ -1,73 +1,120 @@
-import React, { Component } from "react";
-import WhiteLogo from "../images/logo.jpg";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import WhiteLogo from '../images/logo.jpg'
 
-export default class Terms extends Component {
-  render() {
+
+export default function Terms() {
     return (
-      <>
-        <body className="legal-body">
-          <section className="legal-section">
-            <div className="legal-container">
-              <div className="legal-intro">
-                <img
-                  src={WhiteLogo} //MLO Support Logo
-                  width="200"
-                  height="100"
-                  className="company-logo"
-                  alt="Logo"
-                />
-                <h1 className="legal-title">MLO SUPPORT TERMS OF USE</h1>
-                <p>Last Updated:</p>
-              </div>
-            </div>
-          </section>
+    <Container style={{maxWidth: '800px'}}>
+      <Card border="light">
+        <Card.Body className="company-logo">
+            <img
+            src={WhiteLogo} //MLO Support Logo
+            width="150"
+            height="70"
+            className="Nav-Logo"
+            alt="MLO Support">
+           </img>
+        </Card.Body>
+        <Card.Body>
+          <Card.Title className="mb-4 text-center">MLO Support Terms of Use</Card.Title>
+         
+         <Card.Subtitle className="mb-2 text-muted">1. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+        
 
-          <section className="legal-section">
-            <div className="legal-container">
-              <div className="legal-content">
-                <h3>Heading 1</h3>
-                <p>Content of Heading 1</p>
-              </div>
-            </div>
-          </section>
+         <Card.Subtitle className="mb-2 text-muted">2. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
 
-          <section className="legal-section">
-            <div className="legal-container">
-              <div className="legal-content">
-                <h3>Heading 2</h3>
-                <p>Content of heading 2</p>
-              </div>
-            </div>
-          </section>
+          <Card.Subtitle className="mb-2 text-muted">3. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
 
-          <section className="legal-section">
-            <div className="legal-container">
-              <div className="legal-content">
-                <h3>Heading 2</h3>
-                <p>Content of heading 2</p>
-              </div>
-            </div>
-          </section>
+          <Card.Subtitle className="mb-2 text-muted">4. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
 
-          <section className="legal-section">
-            <div className="legal-container">
-              <div className="legal-content">
-                <h3>Heading 2</h3>
-                <p>Content of heading 2</p>
-              </div>
-            </div>
-          </section>
+          <Card.Subtitle className="mb-2 text-muted">5. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
 
-          <section className="legal-section">
-            <div className="legal-container">
-              <div className="legal-ending">
-                <h3></h3>
-                <p></p>
-              </div>
-            </div>
-          </section>
-        </body>
-      </>
+          <Card.Subtitle className="mb-2 text-muted">6. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+
+          <Card.Subtitle className="mb-2 text-muted">7. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+
+          <Card.Subtitle className="mb-2 text-muted">8. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+
+          <Card.Subtitle className="mb-2 text-muted">9. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+
+          <Card.Subtitle className="mb-2 text-muted">10. Section of the Text</Card.Subtitle>
+          <Card.Text classname="mb-4">
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+
+         </Card.Body> 
+         <Card.Text className="mb-2 text-center">MLO Support | A Liberty Wholesale Company</Card.Text>
+      </Card>
+            
+      </Container>
     );
   }
-}
