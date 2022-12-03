ee66c5dc57342fcfee817d680b46395c4528fdd8
Negin Mahrang <55268580+neginmhg@users.noreply.github.com>
2022-12-03 02:21:55 -0800

    Edited Notes sections

--- dashboard-website/mortgage_dashboard/frontend/src/components/Table.jsx
+++ dashboard-website/mortgage_dashboard/frontend/src/components/Table.jsx
@@ -5,7 +5,12 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import api from "../services/api";
-
+import axios from 'axios';
+import { saveAs } from 'file-saver';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 const TableComponent = ({ url, page, data, column, notes }) => {
   const [noteText, setNoteText] = useState("");
   /**
@@ -17,6 +22,7 @@ const TableComponent = ({ url, page, data, column, notes }) => {
       var formData = new FormData();
       formData.append("note", noteText);
       formData.append("borrower", caseId);
+      // let api=`${process.env.REACT_APP_API_URL}/api/borrowernote/`
       try {
         const response = await api({
           method: "post",
@@ -43,7 +49,43 @@ const TableComponent = ({ url, page, data, column, notes }) => {
         console.log(error);
       }
     }
-  };
+  }
+  const handleDeleteNote = async (caseId) => {
+  // store the states in the form data
+  if (page === "Borrowers") {
+    var formData = new FormData();
+    formData.append("borrower", caseId)
+    api="http://localhost:8000/api/borrowernote-list/"
+    try {
+      const response = await axios({
+        method: "DELETE",
+        url: api,
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      window.location.reload(false);
+    
+    } catch(error) {
+      console.log(error)
+    }
+  }else if (page === "Leads") {
+    var formData = new FormData();
+    formData.append("lead", caseId)
+    api="http://localhost:8000/api/leadnote-list/"
+    try {
+      const response = await axios({
+        method: "DELETE",
+        url: api,
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      window.location.reload(false);
+    } catch(error) {
+      console.log(error)
+    }
+  }
+  
+}
   // State variable to keep track of all the expanded rows
   const [expandedRows, setExpandedRows] = useState([]);
 
@@ -75,29 +117,24 @@ const TableComponent = ({ url, page, data, column, notes }) => {
           <tr className="table-title">List of {page}</tr>
           <tr className="table-heading">
             {column.map((item, index) => (
-              <TableHeadItem item={item} url={url} page={page} />
+              <TableHeadItem key={page+"heading"+index} item={item} url={url} page={page} />
             ))}
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
-            <TableRow
-              page={page}
-              item={item}
-              column={column}
-              notes={notes}
-              index={index}
-              expandedRows={expandedRows}
-              handleEpandRow={handleEpandRow}
-              expandState={expandState}
-              handleAddNote={handleAddNote}
-              setNoteText={setNoteText}
-            />
-          ))}
-        </tbody>
-        <tr className="last-table-row">
-          Showing {data.length} out of {data.length} results
-        </tr>
+           {data.map((item, index) => <TableRow key={page+"tableRow"+index}
+            page={page} item={item} column={column} notes={notes} index={index} 
+            expandedRows={expandedRows} handleEpandRow={handleEpandRow} 
+            expandState={expandState}
+            handleAddNote={handleAddNote}
+            handleDeleteNote={handleDeleteNote}
+            setNoteText ={setNoteText}
+          />)}
+      
+      <tr className="last-table-row">
+        Showing {data.length} out of {data.length} results 
+      </tr>
+      </tbody>
       </Table>
     
   );
@@ -106,7 +143,7 @@ const TableHeadItem = ({ item, url, page }) => {
   if (item.heading === "AddRow") {
     return (
       <th>
-        <AddRow page={page} url={url} />
+       <AddRow page={`${page}`} api={`${api}`} />
       </th>
     );
   } else {
@@ -115,58 +152,42 @@ const TableHeadItem = ({ item, url, page }) => {
 };
 
 const TableRow = ({
-  page,
-  item,
-  column,
-  notes,
-  index,
-  expandedRows,
-  handleEpandRow,
-  expandState,
-  handleAddNote,
-  setNoteText,
+  page, item, column, notes, index, expandedRows, handleEpandRow, expandState,
+  handleAddNote,handleDeleteNote, setNoteText 
 }) => (
   <>
     <tr id={index}>
       {column.map((columnItem) => {
         if (columnItem.value.includes(".")) {
           const itemSplit = columnItem.value.split(".");
-          return <td>{item[itemSplit[0]][itemSplit[1]]}</td>;
+          return <td key={page+index}>{item[itemSplit[0]][itemSplit[1]]}</td>;
         }
         if (columnItem.heading === "AddRow") {
-          return (
-            <th>
-              <ActionBtn page={`${page}`} rowData={item} index={index} />
-            </th>
-          );
+           return (<th key={page+"addRow"+index}><ActionBtn page={`${page}`} rowData={item} index={index} /></th>);
         } else if (columnItem.heading === "Details") {
-          return (
-            <th>
-              {" "}
-              <Button
-                variant="link"
-                value={index}
-                onClick={(event) => handleEpandRow(event, item.caseId)}
-              >
-                {expandState[item.caseId] ? "Hide" : "Show"}
-              </Button>{" "}
-            </th>
-          );
+          return (<th key={page+"detail"+index}> <Button variant="link" 
+          value={index}  onClick={event => handleEpandRow(event, item.caseId)}>
+            {
+              expandState[item.caseId] ?
+                'Hide' : 'Show'
+            }</Button> </th>);
         } else if (columnItem.heading === "Link") {
-          return (
-            <td>
-              {" "}
-              <Button variant="link" onClick="#">
-                download
-              </Button>{" "}
-            </td>
-          );
+          return (<td key={page+columnItem.heading+index}> <Button variant="link"
+          onClick={(e) => window.open( item[`${columnItem.value}`], "_blank")}
+         >
+           Open
+        </Button>
+        <Button variant="link" 
+            onClick={(e) =>saveAs(item[`${columnItem.value}`], 'image.jpg')}>
+            Download
+          </Button>
+        </td>);
         } else {
-          if (columnItem.heading === "Date") {
-            return <td>{item[`${columnItem.value}`].slice(0, 10)}</td>;
-          }
-          return <td>{item[`${columnItem.value}`]}</td>;
+        if(columnItem.heading==="Date") {
+          return <td key={page+"date"+index}>{item[`${columnItem.value}`].slice(0, 10)}</td>
         }
+        return <td key={page+"_"+columnItem.value+"_row"+index}>{item[`${columnItem.value}`]}</td>
+      }
       })}
     </tr>
     <>
@@ -177,45 +198,41 @@ const TableRow = ({
             style={{ backgroundColor: "#343A40", color: "#FFF" }}
           >
             {notes.map((note) => {
-              if (page === "Borrowers") {
-                if (note.borrower === item.caseId) {
-                  return (
-                    <>
-                      <li>
-                        {note.borrowernote}
-                        <small> ({note.created_on})</small>
-                      </li>
+              if (page === "Borrowers"){
+                  if (note.borrower === item.caseId) {
+                    return <>
+                      <Row key={page+note+index}>
+                        <Col xs="auto"><div><h6>{note.borrowernote}
+                        <small> ({note.created_on})</small></h6></div></Col>
+                        <Col xs lg="2">
+                         <Button onClick={event => handleDeleteNote(item.caseId)}variant="outline-danger"><FontAwesomeIcon icon={faTrash} /></Button>
+                        </Col>
+                      </Row>
                     </>
-                  );
-                }
-              } else if (page === "Leads") {
-                if (note.lead === item.caseId) {
-                  return (
-                    <>
-                      <li>
-                        {note.leadnote}
-                        <small> ({note.created_on})</small>
-                      </li>
+                  }
+              } else if (page === "Leads"){
+                  if (note.lead === item.caseId) {
+                    return <>  
+                     <Row key={page+note+index}>
+                        <Col xs="auto"><div><h6>{note.leadnote}
+                        <small> ({note.created_on})</small></h6></div></Col>
+                        <Col xs lg="2">
+                         <Button onClick={event => handleDeleteNote(item.caseId)}variant="outline-danger"><FontAwesomeIcon icon={faTrash} /></Button>
+                        </Col>
+                      </Row>
                     </>
-                  );
-                }
+                  }
               }
             })}
             <Form>
               <Form.Group className="mb-3" controlId="notesTextarea">
-                <Form.Label>Notes</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  onChange={(e) => setNoteText(e.target.value)}
-                />
-              </Form.Group>
-              <Button
-                variant="outline-light"
-                onClick={(event) => handleAddNote(item.caseId)}
-              >
-                Save
-              </Button>
+                  <Form.Label>Notes</Form.Label>
+                  <Form.Control as="textarea" rows={3}
+                  onChange={(e) => setNoteText(e.target.value)}/>
+                </Form.Group>
+                <Button variant="outline-light" onClick={event => handleAddNote(item.caseId)}>
+                  Save
+                </Button>
             </Form>
           </td>
         </tr>
